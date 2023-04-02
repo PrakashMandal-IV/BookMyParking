20a5846221358fbfd5ff78ea3d3bba528e3ac05e
Prakash Mandal <prakashmandal.iv@gmail.com>
2023-04-02 16:35:15 +0530

    Add : dashboard layout

--- src/Pages/AdminDashboard/DashboardLayout.jsx
+++ src/Pages/AdminDashboard/DashboardLayout.jsx
@@ -1,7 +1,21 @@
-const AdminDashboardLayout=()=>{
+import { Outlet } from "react-router-dom"
+
+const AdminDashboardLayout = () => {
     return (<>
         <div className="w-full h-full flex">
-            <div className="w-40  bg-black"></div>
+            <div className="w-60  bg-gray-200">
+                <div className="flex flex-col mt-5">
+                    <div className="text-xl pl-10 font-medium hover:bg-gray-300 py-4">
+                        Overview
+                    </div>
+                    <div className="text-xl pl-10  font-medium hover:bg-gray-300 py-4">
+                        Organization
+                    </div>
+                </div>
+            </div>
+            <div className="flex-grow">
+                <Outlet />
+            </div>
         </div>
     </>)
 }
