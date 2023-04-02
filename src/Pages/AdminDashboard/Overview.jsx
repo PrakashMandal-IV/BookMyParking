15785063a259b1973c087c6a3eacd6c08df39040
Prakash Mandal <prakashmandal.iv@gmail.com>
2023-04-02 16:54:44 +0530

    Add : current date

--- src/Pages/AdminDashboard/Overview.jsx
+++ src/Pages/AdminDashboard/Overview.jsx
@@ -1,6 +1,30 @@
-const OverView=()=>{
-    return(<>
-      over view
+import { useEffect, useState } from "react";
+
+const OverView = () => {
+    const [CurrentDate, SetCurrentDate] = useState('')
+    useEffect(() => {
+        const currentDate = new Date();
+        SetCurrentDate(formatDate(currentDate))
+    }, [])
+
+
+    function formatDate(date) {
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        const month = months[date.getMonth()];
+        const day = date.getDate();
+        const year = date.getFullYear();
+        return `${day} ${month}, ${year}`;
+    }
+
+
+
+    return (<>
+        <div className="pl-10 pr-20 pt-5 ">
+            <div className="flex h-10">
+                <div className="text-2xl">Overview</div>
+                <div className="ml-auto text-lg font-medium">{CurrentDate}</div>
+            </div>
+        </div>
     </>)
 }
 export default OverView
