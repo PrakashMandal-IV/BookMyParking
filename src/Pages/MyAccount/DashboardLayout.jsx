fb1e2727e57550c2db4b9e17eb17dd0022c6a4b1
Prakash Mandal <prakashmandal.iv@gmail.com>
2023-04-15 21:58:24 +0530

    Add : GetParking

--- src/Pages/MyAccount/DashboardLayout.jsx
+++ src/Pages/MyAccount/DashboardLayout.jsx
@@ -20,19 +20,19 @@ const MyAccountDashboardLayout = (props) => {
                             <p className="text-center text-lg text-white">{props?.UserData?.FullName}</p>
                         </div>
                     </div>
-                    <div className="text-lg pl-10 text-white font-medium hover:bg-slate-800 py-4 cursor-pointer transition-all" onClick={() => nav('')}>
+                    <div className="text-lg pl-10 text-white  hover:bg-slate-800 py-4 cursor-pointer transition-all" onClick={() => nav('')}>
                         Account
                     </div>
-                    <div className="text-lg pl-10  text-white font-medium hover:bg-slate-800 py-4 cursor-pointer transition-all" onClick={() => nav('bookings')}>
+                    <div className="text-lg pl-10  text-white  hover:bg-slate-800 py-4 cursor-pointer transition-all" onClick={() => nav('bookings')}>
                         Bookings
                     </div>
-                    <div className="text-lg pl-10 text-white font-medium hover:bg-slate-800 py-4 cursor-pointer transition-all" onClick={() => nav('')}>
+                    <div className="text-lg pl-10 text-white  hover:bg-slate-800 py-4 cursor-pointer transition-all" onClick={() => nav('')}>
                         Vehicles
                     </div>
-                    <div className="mt-auto  flex gap-2 text-lg justify-around  text-red-500 font-medium bg-slate-800 hover:bg-slate-900 py-4 cursor-pointer transition-all" onClick={() => Logout()}>
-                        <p className="my-auto">Logout</p>
+                    <div className="mt-auto  flex gap-2 text-lg justify-around  text-red-500  bg-red-500 hover:bg-red-600 py-4 cursor-pointer transition-all" onClick={() => Logout()}>
+                        <p className="my-auto text-white">Logout</p>
                         <div className="">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} className="w-8 h-8 stroke-red-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} className="w-8 h-8 stroke-white">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
                             </svg>
                         </div>
--- src/Pages/MyAccount/MyBookings.jsx
+++ src/Pages/MyAccount/MyBookings.jsx
@@ -1,8 +1,44 @@
-const MyBookings=()=>{
-    return(<>
-    <div className="">
-
-    </div>
+const MyBookings = () => {
+    return (<>
+        <div className="mx-80 mt-16">
+            <div className="w-full h-32">
+                <p className="font-semi
+                bold text-2xl text-center">Bookings</p>
+                <div className="mt-5">
+                    <div className="flex flex-col gap-4">
+                        <BookingCard />
+                        <BookingCard />
+                    </div>
+                </div>
+            </div>
+        </div>
     </>)
 }
 export default MyBookings
+
+
+const BookingCard = () => {
+    return (<>
+        <div className="bg-gradient-to-l    border hover:border-slate-700 transition-all w-full flex gap-2 p-2 rounded-md">
+
+            <div className="max-w-[35%] sm:max-w-[40%] overflow-hidden flex">
+                <img src="https://i.pinimg.com/564x/7e/6a/f9/7e6af90864bb1ba1ac9f87a6f31cc3a5.jpg" className="rounded-md my-auto sm:max-h-52 object-cover pointer-events-none" alt="" />
+            </div>
+            <div className="flex-grow flex flex-col">
+                <div className="flex w-full">
+                    <div className="">
+                        <p className="text-2xl text-slate-700">PVR <span className="text-lg font-light">(10:00 AM - 12:00 PM)</span></p>
+                        <p className="text-slate-700">123 streets , raipur </p>
+                        <p className=" text-green-600 mt-2">Total Cost : Rs 100 only</p>
+                    </div>
+                    <div className=" ml-auto">
+                      <p className="text-slate-700 ">4 wheeler | JK029080 | Safari</p>
+                    </div>
+                </div>
+                <div className="mt-auto">
+                    Rating
+                </div>
+            </div>
+        </div>
+    </>)
+}
