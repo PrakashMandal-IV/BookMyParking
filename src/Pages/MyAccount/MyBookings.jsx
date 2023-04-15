7260416faed4f61f2d0dfd11b78fb397143d6c14
Prakash Mandal <prakashmandal.iv@gmail.com>
2023-04-15 22:52:27 +0530

    Add : Get booking list and ratings

--- src/Pages/MyAccount/MyBookings.jsx
+++ src/Pages/MyAccount/MyBookings.jsx
@@ -1,4 +1,23 @@
+import { useEffect, useState } from "react"
+import { Api, Get } from "../../components/Api"
+import StarRatings from "react-star-ratings"
+
 const MyBookings = () => {
+  const [ParkingList,SetParkingList] = useState([])
+useEffect(()=>{
+    GetParkingList()
+},[])
+
+    const GetParkingList=async()=>{
+        var det = {
+            "link": "Customer/GetBookingList"
+        }
+        Get(det, (res, rej) => {
+            SetParkingList(res.data)
+        }, (err) => {
+
+        });
+    }
     return (<>
         <div className="mx-80 mt-16">
             <div className="w-full h-32">
@@ -6,8 +25,11 @@ const MyBookings = () => {
                 bold text-2xl text-center">Bookings</p>
                 <div className="mt-5">
                     <div className="flex flex-col gap-4">
-                        <BookingCard />
-                        <BookingCard />
+                        {ParkingList.map((item,idx)=>(
+                            <BookingCard item={item} key={idx}/>
+                        ))}
+                        
+                        
                     </div>
                 </div>
             </div>
@@ -17,26 +39,52 @@ const MyBookings = () => {
 export default MyBookings
 
 
-const BookingCard = () => {
+const BookingCard = (props) => {
+    const [Rating,SetRating] = useState(props.item.Ratings)
+
+
+    const UpdateUserRating=(value)=>{
+       if(value>0){
+        var det = {
+            "link": "Customer/SetUserRating?BookingID="+props.item.BookingID+"&Rating="+value
+        }
+        Get(det, (res, rej) => {
+            SetRating(value)
+        }, (err) => {
+
+        });
+       }
+    }
     return (<>
         <div className="bg-gradient-to-l    border hover:border-slate-700 transition-all w-full flex gap-2 p-2 rounded-md">
 
             <div className="max-w-[35%] sm:max-w-[40%] overflow-hidden flex">
-                <img src="https://i.pinimg.com/564x/7e/6a/f9/7e6af90864bb1ba1ac9f87a6f31cc3a5.jpg" className="rounded-md my-auto sm:max-h-52 object-cover pointer-events-none" alt="" />
+                <img src={Api + "orgthumb?OrgID=" + props.item.OrganizationID + "&FileName=" + props.item.Thumbnail} className="rounded-md my-auto sm:max-h-52 object-cover pointer-events-none" alt="" />
             </div>
             <div className="flex-grow flex flex-col">
                 <div className="flex w-full">
                     <div className="">
-                        <p className="text-2xl text-slate-700">PVR <span className="text-lg font-light">(10:00 AM - 12:00 PM)</span></p>
-                        <p className="text-slate-700">123 streets , raipur </p>
-                        <p className=" text-green-600 mt-2">Total Cost : Rs 100 only</p>
+                        <p className="text-2xl text-slate-700">{props.item.OrganizationName} <span className="text-lg font-light">(10:00 AM - 12:00 PM)</span></p>
+                        <p className="text-slate-700">{props.item.City} , {props.item.State} </p>
+                        <p className=" text-green-600 mt-2 text-sm">Total Cost : Rs {props.item.Amount} only</p>
                     </div>
                     <div className=" ml-auto">
-                      <p className="text-slate-700 ">4 wheeler | JK029080 | Safari</p>
+                      <p className="text-slate-700 ">{props.item.VehicalType} | {props.item.VehicalNumber}|  {props.item.VhicalName}</p>
                     </div>
                 </div>
                 <div className="mt-auto">
-                    Rating
+                    <p className="">Rating</p>
+                    <div className="">
+                    <StarRatings
+                            rating={Rating}
+                            starDimension="25px"
+                            starRatedColor="#475569"
+                            starHoverColor="#334155"
+                            changeRating={UpdateUserRating}
+                            numberOfStars={5}
+                            name='User Rating'
+                        />
+                    </div>
                 </div>
             </div>
         </div>
